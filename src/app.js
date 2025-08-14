ab161a69eade3c305b3ff4b2293bfc4fa232aaff
1v4nP4 <1v4nP4@proton.me>
2025-08-14 13:18:40 +0000

    primer commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,34 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronombres = ['el', 'nuetro'];
+  let adjetivos = ['gran', 'super'];
+  let sustantivos = ['dragon', 'mapache'];
+
+  //Generador de nombres
+  function generadorDominios(pronombres,adjetivos,sustantivos){
+    const combinaciones = [];
+
+    pronombres.forEach(pronombre => {
+      adjetivos.forEach(adjetivo => {
+        sustantivos.forEach(sustantivo =>{
+          const dominio = `${pronombre}${adjetivo}${sustantivo}.com`;
+          combinaciones.push(dominio);
+        });
+      });
+    });
+    return combinaciones;
+  }
+  //Funcion para mostrar por consola
+  function mostrarDominios(dominios){
+    console.log("Dominios:");
+    dominios.forEach(dominio => console.log(dominio));
+  }
+
+ //Función para ejecutar el generador
+  function ejecutarGenerador() {
+  const dominios = generadorDominios(pronombres, adjetivos, sustantivos);
+  mostrarDominios(dominios);
+}
+ejecutarGenerador();
 };
